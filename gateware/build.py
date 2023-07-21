bc4ddea45628e0a491fd170d81f6665605a9c002
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2023-07-20 23:31:56 -0400

    build: add more I/O resources

--- gateware/build.py
+++ gateware/build.py
@@ -3,6 +3,7 @@ import argparse
 from amaranth.build import *
 from amaranth_boards.tinyfpga_bx import *
 from amaranth_boards.upduino_v3 import *
+from amaranth_boards.resources import *
 
 import toplevel
 from vgatimings import TIMINGS
@@ -13,12 +14,30 @@ class PlatformData():
     def __init__(self, name):
         if name == "upduino":
             self.platform = UpduinoV3Platform()
-            self.platform.add_resources(
-                    [Resource("clk12e", 0, Pins("35", dir="i"),
-                              Clock(12e6), Attrs(IO_STANDARD="SB_LVCMOS"))])
+            self.platform.add_resources([
+                Resource("clk12e", 0, Pins("35", dir="i"), Clock(12e6),
+                    Attrs(IO_STANDARD="SB_LVCMOS")),
+                VGAResource(0, r="2 46 47 45 48", g="3 4 44 6 9", b="28 38 42 36 43",
+                    hs="11", vs="18", attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+                UARTResource(0, rx="34", tx="37",
+                    attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+                PS2Resource(0, clk="19", dat="13",
+                    attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+            ])
             self.clkresource = "clk12e"
         elif name == "tinyfpga":
             self.platform = TinyFPGABXPlatform()
+            self.platform.add_resources([
+                VGAResource(0, r="1 2 3 4", g="5 6 7 8", b="9 10 11 12", hs="13", vs="14",
+                    conn=("gpio", 0), attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+                UARTResource(0, rx="23", tx="24",
+                    conn=("gpio", 0), attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+                PS2Resource(0, clk="21", dat="22",
+                    conn=("gpio", 0), attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+                *LEDResources("dbgled", pins="15 16 17 18",
+                    conn=("gpio", 0), attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+            ])
+
             self.clkresource = "clk16"
         else:
             raise Exception("Unknown platform", name)
