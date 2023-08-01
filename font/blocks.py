d1cc7a83a00d726fc424715918219b925a71edfa
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2023-08-01 17:37:24 -0600

    font: add a font builder
    This takes one or more .hex files with font data and produces binary
    blobs with single-wide and double-wide characters, as well a character
    map. These are produced in two variants: one with the full set of
    supported characters and a second one with a stripped down set for parts
    with limited flash space.

---
+++ font/blocks.py
@@ -0,0 +1,96 @@
+import functools
+
+def block(start, end):
+    return set(range(start, end + 1))
+
+CHARSETS_CORE = [
+    block(32,126),                      # Basic Latin
+    block(0xa0,0xff) - {0xad},          # Latin-1, minus soft hyphen control code
+    block(0x100,0x17f),                 # Latin Extended A
+    block(0x180,0x24f),                 # Latin Extended B
+    block(0x250,0x2ff),                 # IPA extensions, spacing modifier letters
+    # greek
+    block(0x370,0x3ff) - {0x378, 0x379, 0x380, 0x381, 0x382, 0x383, 0x38b, 
+0x38d, 0x3a2},
+    block(0x400,0x4ff) - block(0x483,0x489), # Cyrillic, non-combiners
+    block(0x500,0x52f),                 # more cyrillic
+    block(0x531,0x58f) - {0x557, 0x558, 0x58b, 0x58c}, # Armenian
+    block(0x5d0,0x5ea) | {0x5c0, 0x5c3, 0x5c6} | block(0x5ef, 0x5f4), # Hebrew
+    block(0x10a0,0x10ff) - block(0x10c8, 0x10cc) - {0x10c6, 0x10ce, 0x10cf}, # georgian
+    block(0x1c80, 0x1c88),              # cyrillic extended C
+    block(0x1c90, 0x1cbf) - {0x1cbb, 0x1cbc}, # georgian extended
+    block(0x1d00, 0x1d7f),              # phonetic extensions
+    block(0x1d80, 0x1dbf),              # phonetic extensions supplement
+    block(0x1e00, 0x1eff),              # Latin extended additional
+    # greek extended
+    block(0x1f00, 0x1ffe) - {0x1f16, 0x1f17, 0x1f1e, 0x1f1f, 0x1f46, 0x1f47, 0x1f4e, 0x1f4f,
+        0x1f58, 0x1f5a, 0x1f5c, 0x1f5e, 0x1f7e, 0x1f7f, 0x1fb5, 0x1fc5, 0x1fd4, 0x1fd5,
+        0x1fdc, 0x1ff0, 0x1ff1, 0x1ff5},
+    block(0x2010, 0x2027) | block(0x2030, 0x205f), # general punctuation
+    block(0x2070, 0x209c) - {0x2072, 0x2073, 0x208f}, # super and subscripts
+    block(0x20a0, 0x20c0),              # currency symbols
+    block(0x2100, 0x214f),              # letterlike symbols
+    block(0x2150, 0x218b),              # number forms
+    block(0x2190, 0x21ff),              # arrows
+    block(0x2200, 0x22ff),              # mathematical operators
+    block(0x2300, 0x23ff),              # miscallaneous technical
+    block(0x2400, 0x2426),              # control pictures
+    block(0x2440, 0x244a),              # optical character recognition
+    block(0x2460, 0x24ff),              # enclosed alphanumerics
+    block(0x2500, 0x25ff),              # box drawing etc.
+    block(0x2600, 0x26ff),              # miscellaneous symbols
+    block(0x27c0, 0x27ef),              # misc mathematical symbols A
+    block(0x27f0, 0x27ff),              # supplemental arrows A
+    block(0x2800, 0x28ff),              # braille patterns
+    block(0x2900, 0x297f),              # supplemental arrows B
+    block(0x2980, 0x29ff),              # misc mathematical symbols B
+    block(0x2a00, 0x2aff),              # supplemental math operators
+    block(0x2b00, 0x2bff) - {0x2b74, 0x2b75, 0x2b96}, # misc symbols and arrows
+    block(0x2c00, 0x2c5f),              # glagolitic
+    block(0x2c60, 0x2c7f),              # Latin extended C
+    block(0x2c80, 0x2cee) | {0x2cf2, 0x2cf3} | block(0x2cf9, 0x2cff), # coptic
+    block(0x2d00, 0x2d25) | {0x2d27, 0x2d27}, # georgian supplement
+    block(0x2d30, 0x2d67) | {0x2d6f, 0x2d70}, # Tifinagh
+    block(0x3000, 0x303f),              # CJK Symbols and punctuation
+    block(0x3041, 0x309f) - block(0x3097, 0x309a), # hiragana
+    block(0x30a0, 0x30ff),              # katakana
+    block(0x3131, 0x318e),              # hangul compatibility jamo
+    block(0x3190, 0x319f),              # kanbun
+    block(0x31f0, 0x31ff),              # katakana phonetic extensions
+    block(0x3300, 0x33ff),              # CJK compatibility
+    block(0x4dc0, 0x4dff),              # yijing hexagram symbols
+    block(0xa4d0, 0xa4ff),              # Lisu
+    block(0xa500, 0xa62b),              # Vai
+    block(0xa6a0, 0xa6f7) - {0xa6f0, 0xa6f1}, # bamum
+    block(0xa640, 0xa66e) | block(0xa680, 0xa69d), # cyrillic extended B
+    block(0xa700, 0xa71f),              # tone letters
+    block(0xa720, 0xa7ca) | block(0xa7d0, 0xa7d9) |
+       block(0xa7f2, 0xa7ff) - {0xa7d2, 0xa7d4}, # latin extended D
+    block(0xa840, 0xa877),              # phags-pa
+    block(0xa960, 0xa97c),              # hangul jamo extended A
+    block(0xab30, 0xab6b),              # latin extended E
+    block(0xac00, 0xd7a3),              # hangul syllables
+    block(0xd7b0, 0xd7bf) - block(0xd7c7, 0xd7ca), # hangul jamo extended B
+    block(0xfb00, 0xfb06) | block(0xfb13, 0xfb17) | # alphabetic presentation forms
+       block(0xfb1d, 0xfb4f) - {0xfb1e, 0xfb37, 0xfb3d, 0xfb3f, 0xfb42, 0xfb45}, 
+    block(0xfe10, 0xfe19),              # vertical forms
+        {0xfffd},
+]
+
+CHARSETS_EXTENDED = [
+    block(0x3105, 0x312f),              # bopomofo
+    block(0x31a0, 0x31bf),              # bopomofo extended
+    block(0x31c0, 0x313e),              # CJK strokes
+    block(0x3200, 0x32ff) - {0x321f},   # enclosed CJK letters and months
+    
+]
+
+CHARSETS_EMOJI = [ 
+# TODO
+]
+
+CHARS_CORE = sorted(functools.reduce(lambda a, b: a.union(b), CHARSETS_CORE))
+CHARS_FULL = sorted(functools.reduce(lambda a, b: a.union(b), CHARSETS_CORE + CHARSETS_EXTENDED, set()))
+CHARS_EMOJI = sorted(functools.reduce(lambda a, b: a.union(b), CHARSETS_EMOJI, set()))
+
+__all__ = ["CHARS_CORE", "CHARS_FULL", "CHARS_EMOJI"]
---
+++ font/build_font.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import os, pathlib, struct, sys
+
+from blocks import *
+
+"""
+Build the font binaries to be flashed. The binaries are output to the build/
+directory. There are two variants: a reduced character set for parts with 1M
+of flash, and the full character set covering most of the Unicode characters
+that can be represented in a character grid.
+Each variant produces 3 files:
+* font1-$variant.bin for single-wide characters
+* font2-$variant.bin for double-wide characters
+* charmap-$variant.bin as a character map, mapping unicode codepoints to
+  indices into the above files.
+"""
+class Char():
+    def __init__(self, code, hexdata):
+        if len(hexdata) == 32:
+            self.type = "single"
+        elif len(hexdata) == 64:
+            self.type = "double"
+        else:
+            raise Exception("Bad character length %d" % len(hexdata))
+
+        self.data = bytearray() 
+        if self.type == "single":
+            for i in range(0, len(hexdata), 2):
+                self.data.append(int('{:08b}'.format(int(hexdata[i:i+2], 16))[::-1],2))
+        else:
+            for i in range(0, len(hexdata), 4):
+                datum = int('{:016b}'.format(int(hexdata[i:i+4], 16))[::-1], 2)
+                self.data.append(datum & 0xff)
+                self.data.append(datum >> 8)
+
+        self.code = code
+
+def read_hex(chars, charset, infile):
+    for line in infile:
+        (hexcode, data) = line.rstrip().split(":")
+        charnum = int(hexcode, 16)
+        if charnum in charset and charnum not in chars:
+            chars[charnum] = Char(charnum, data)
+
+def write_blobs(chars, charset, *, singlefile, doublefile, charmapfile):
+    single_idx = double_idx = emoji_idx = 0
+    charmap = [0xffff] * 65536
+    # TODO: deduplicate images
+    for chnum in charset:
+        if chnum not in chars: continue
+        char = chars[chnum]
+        if char.type == 'single':
+            # write single
+            if single_idx >= 16384:
+                raise Exception("Too many single-wides!")
+            charmap[char.code] = single_idx
+            single_idx = single_idx + 1
+            singlefile.write(char.data)
+        elif char.type == 'double':
+            if double_idx >= 32768:
+                raise Exception("Too many double-wides!")
+            charmap[char.code] = double_idx + 16384
+            double_idx = double_idx + 1
+            doublefile.write(char.data)
+        else:
+            raise Exception("Uknown character type")
+    # write charmap
+    for ch in charmap:
+        charmapfile.write(struct.pack('<H', ch))
+
+WARNING = "\e[1;31mWARNING\e[0m"
+
+def main():
+    chars = {}
+    status = 0
+
+    if len(sys.argv) < 2:
+        print("usage: build.py fontfile.hex [fontfile.hex ...]")
+        sys.exit(1)
+    for fn in sys.argv[1:]:
+        f = open(fn, "r")
+        read_hex(chars, CHARS_FULL, f)
+
+    builddir = pathlib.Path("build")
+    if not builddir.exists():
+        builddir.mkdir()
+
+    with open(builddir/"font1-core.bin", "wb") as singles, \
+         open(builddir/"font2-core.bin", "wb") as doubles, \
+         open(builddir/"charmap-core.bin", "wb") as charmap:
+        write_blobs(chars, CHARS_CORE, singlefile=singles,
+                    doublefile=doubles, charmapfile=charmap)
+        print("core chars:")
+        print(f" singles: {singles.tell()} bytes," +
+              f" doubles: {doubles.tell()} bytes")
+        if doubles.tell() > 512*1024:
+            print(WARNING + " doublewide font too big! Packing will fail.")
+            status = 1
+        if doubles.tell() + singles.tell() + charmap.tell() > 704*1024:
+            print(WARNING + " total size too big! Packing will fail.")
+            status = 1
+
+    with open(builddir/"font1-full.bin", "wb") as singles, \
+         open(builddir/"font2-full.bin", "wb") as doubles, \
+         open(builddir/"charmap-full.bin", "wb") as charmap:
+        write_blobs(chars, CHARS_FULL, singlefile=singles,
+                    doublefile=doubles, charmapfile=charmap)
+        print("full charset:")
+        print(f" singles: {singles.tell()} bytes," +
+              f" doubles: {doubles.tell()} bytes")
+
+    sys.exit(status)
+
+if __name__ == "__main__":
+    main()
