5647e1ff3147c252cb0f57be5d3d6b6778f440ae
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2023-08-16 10:57:19 -0600

    Add a cursor, with blinking

---
+++ gateware/cursor.py
@@ -0,0 +1,94 @@
+from amaranth import *
+from amaranth.lib.wiring import *
+import enum
+
+__all__ = ["CursorShape", "CursorControlsSig", "Cursor"]
+class CursorShape(enum.Enum):
+    UNDERLINE = 1
+    SOLID = 2
+    VERTICAL = 3
+    BOX = 4
+
+class CursorControlsSig(Signature):
+    def __init__(self, *, rows, cols):
+        return super().__init__({
+            "cursor_x": Out(range(cols)),
+            "cursor_y": Out(range(rows)),
+            "shape": Out(CursorShape),
+            "blink": Out(1, reset = 1),
+            "doublewide": Out(1),
+        })
+
+class Cursor(Elaboratable):
+    def __init__(self, timings):
+        self.signature = Signature({
+            "controls": In(CursorControlsSig(rows=timings.rows, cols=timings.cols)),
+            "hctr": In(timings.hctr_shape()),
+            "vctr": In(timings.vctr_shape()),
+            "output": Out(1),
+        })
+        self.__dict__.update(self.signature.members.create())
+        self.blinkctrval = Const(int(timings.pclk * 1e6 * 0.5))
+        print("blink counter", self.blinkctrval)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        col = Signal.like(self.controls.cursor_x)
+        row = Signal.like(self.controls.cursor_y)
+        ctr = Signal(32)
+        cursor_on = Signal()
+
+        row_pix = self.vctr[0:4]
+        col_pix = self.hctr[0:3]
+        m.d.comb += col.eq(self.hctr >> 3)
+        m.d.comb += row.eq(self.vctr >> 4)
+
+        # counter for the blink
+        with m.If(ctr == self.blinkctrval):
+            m.d.sync += ctr.eq(0)
+            m.d.sync += cursor_on.eq(~cursor_on)
+        with m.Else():
+            m.d.sync += ctr.eq(ctr + 1)
+
+        # output before any blink is applied
+        pre_output = Signal()
+        m.d.comb += pre_output.eq(0)
+
+        # Apply cursor style
+        cursor_left = (col == self.controls.cursor_x)
+        cursor_right = Mux(self.controls.doublewide, col == self.controls.cursor_x + 1, col == self.controls.cursor_x)
+        with m.If((cursor_left | cursor_right) & (row == self.controls.cursor_y)):
+            with m.Switch(self.controls.shape):
+                with m.Case(CursorShape.BOX):
+                    m.d.comb += pre_output.eq(1)
+                with m.Case(CursorShape.VERTICAL):
+                    m.d.comb += pre_output.eq(col_pix == 0)
+                with m.Case(CursorShape.UNDERLINE):
+                    m.d.comb += pre_output.eq((row_pix == 13) | (row_pix == 14))
+                with m.Case(CursorShape.BOX):
+                    m.d.comb += pre_output.eq((col_pix == 0) | (col_pix == 7) |
+                                               (cursor_left & (row_pix == 0)) |
+                                               (cursor_right & (row_pix == 15)))
+
+        m.d.comb += self.output.eq(pre_output & (cursor_on | ~self.controls.blink))
+
+        return m
+
+if __name__ == "__main__":
+    from amaranth.sim import *
+    dut = Cursor()
+    sim = Simulator(dut)
+    sim.add_clock(40e-9)
+    hctr = vctr = 0
+    def proc():
+        yield dut.shape.eq(CursorShape.UNDERLINE)
+        yield dut.blink.eq(0)
+        for vctr in range(0, 16):
+            for hctr in range(0, 100):
+                yield dut.hctr.eq(hctr)
+                yield dut.vctr.eq(vctr)
+                yield Tick()
+    sim.add_sync_process(proc)
+    with sim.write_vcd("waves/cursor.vcd"):
+        sim.run()
--- gateware/toplevel.py
+++ gateware/toplevel.py
@@ -1,5 +1,7 @@
 from amaranth import *
-import icepll, vgasync
+import icepll
+from vgasync import *
+from cursor import *
 
 class Toplevel(Elaboratable):
     """ The top level of the terminal, everything goes under here. """
@@ -14,19 +16,26 @@ class Toplevel(Elaboratable):
         m.submodules.pll = icepll.ICEPLL(f_in, self.timings.pclk * 1e6,
                                       self.pdata.clkresource)
 
-        m.submodules.vgasync = vgs = vgasync.VGASync(self.timings)
+        m.submodules.vgasync = vgs = VGASync(self.timings)
+
+        m.submodules.cursor = cursor = Cursor(self.timings)
+        bgcolor = [23, 20, 31]
+        fgcolor = [31, 31, 31]
+        color = [Mux(cursor.output, c[0], c[1]) for c in zip(fgcolor, bgcolor)]
+        m.d.comb += cursor.controls.shape.eq(CursorShape.UNDERLINE)
+        m.d.comb += cursor.hctr.eq(vgs.hctr)
+        m.d.comb += cursor.vctr.eq(vgs.vctr)
 
         output = platform.request("vga")
         m.d.comb += [
             output.hs.eq(vgs.hs),
             output.vs.eq(vgs.vs),
-            output.r.eq(Mux(vgs.active, 23, 0)),
-            output.g.eq(Mux(vgs.active, 20, 0)),
-            output.b.eq(Mux(vgs.active, 31, 0)),
+            output.r.eq(Mux(vgs.active, color[0], 0)),
+            output.g.eq(Mux(vgs.active, color[1], 0)),
+            output.b.eq(Mux(vgs.active, color[2], 0)),
         ]
 
-        # Not all platforms need the pclk/den outputs, so it's okay if the
-        # request fails.
+        # Not all platforms need the pclk/den outputs, so it's okay if the request fails.
         try:
             pclk = platform.request("pclk")
             m.d.comb += pclk.eq(ClockSignal("sync"))
