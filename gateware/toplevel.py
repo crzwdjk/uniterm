edaf61cdf574ebfeaa023516dbd639597c941e81
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2023-08-17 11:17:11 -0600

    Add a rowbuffer with test pattern
    Create a new videoout module that holds all the logic for video output
    including requesting the VGA resource and compositing the cursor. Also
    adds a rowbuffer. Reading from the rowbuffer takes a cycle so hsync,
    vsync, and den are delayed by a clock on the way to the output. Also,
    this fixes the phase of the clock.

--- gateware/toplevel.py
+++ gateware/toplevel.py
@@ -1,8 +1,9 @@
 from amaranth import *
 from amaranth.lib.wiring import *
-import icepll
-from vgasync import *
-from cursor import *
+import icepll, videoout
+
+# XXX: test only
+from rowbuftest import *
 
 class Toplevel(Elaboratable):
     """ The top level of the terminal, everything goes under here. """
@@ -17,31 +18,14 @@ class Toplevel(Elaboratable):
         m.submodules.pll = icepll.ICEPLL(f_in, self.timings.pclk * 1e6,
                                       self.pdata.clkresource)
 
-        m.submodules.vgasync = vgs = VGASync(self.timings)
+        m.submodules.videoout = out = videoout.VideoOut(self.timings)
 
-        m.submodules.cursor = cursor = Cursor(self.timings)
-        bgcolor = [23, 20, 31]
-        fgcolor = [31, 31, 31]
-        color = [Mux(cursor.output, c[0], c[1]) for c in zip(fgcolor, bgcolor)]
-        m.d.comb += cursor.controls.shape.eq(CursorShape.BOX)
-        connect(m, cursor.pos, vgs.pos)
-
-        output = platform.request("vga")
+        rowbuf = Memory(width = 8, depth = self.timings.cols * 16 * 2, init = ROWBUFTEST)
+        m.submodules.rowbuf_read = rowbuf_read = rowbuf.read_port(transparent = False)
         m.d.comb += [
-            output.hs.eq(vgs.hs),
-            output.vs.eq(vgs.vs),
-            output.r.eq(Mux(vgs.active, color[0], 0)),
-            output.g.eq(Mux(vgs.active, color[1], 0)),
-            output.b.eq(Mux(vgs.active, color[2], 0)),
+            rowbuf_read.addr.eq(out.rowbuf_addr),
+            rowbuf_read.en.eq(out.rowbuf_en),
+            out.rowbuf_data.eq(rowbuf_read.data),
         ]
-
-        # Not all platforms need the pclk/den outputs, so it's okay if the request fails.
-        try:
-            pclk = platform.request("pclk")
-            m.d.comb += pclk.eq(ClockSignal("sync"))
-            den = platform.request("den")
-            m.d.comb += den.eq(vgs.active)
-        except:
-            pass
 
         return m
---
+++ gateware/videoout.py
@@ -0,0 +1,68 @@
+from amaranth import *
+from amaranth.lib.wiring import *
+from signatures import *
+from cursor import *
+from vgasync import *
+
+class VideoOut(Elaboratable):
+    def __init__(self, timings):
+        self.timings = timings
+        self.signature = Signature({
+            "pos": Out(VideoPosSig(timings)),
+            "cursor": In(CursorControlsSig(rows=timings.rows, cols=timings.cols)),
+            "rowbuf_addr": Out(range(timings.cols * 16 * 2)),
+            "rowbuf_en": Out(1),
+            "rowbuf_data": In(8),
+        })
+        self.__dict__.update(self.signature.members.create())
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # Create the VGA sync module and wire up its outputs to the rowbuf addr/en
+        m.submodules.vgasync = vgs = VGASync(self.timings)
+        connect(m, transpose(self.pos), vgs.pos)
+        m.d.comb += [
+            self.rowbuf_addr.eq(vgs.pos.vctr[0:5] * self.timings.cols + vgs.pos.hctr[3:]),
+            self.rowbuf_en.eq(vgs.active),
+        ]
+
+        active1 = Signal()
+        fetched_bit = self.rowbuf_data.bit_select((vgs.pos.hctr[0:3] - 1)[0:3], 1),
+        m.d.sync += active1.eq(vgs.active)
+
+        # create the cursor and override the shape for now.
+        m.submodules.cursor = cursor = Cursor(self.timings)
+        connect(m, self.cursor, transpose(cursor.controls))
+        m.d.comb += cursor.controls.shape.eq(CursorShape.BOX)
+        cursorval = Signal()
+        m.d.sync += cursorval.eq(cursor.output)
+
+        #bgcolor = [23, 20, 31]
+        bgcolor = [0, 0, 0]
+        fgcolor = [31, 31, 31]
+        color = [Mux(cursorval ^ fetched_bit, c[0], c[1]) for c in zip(fgcolor, bgcolor)]
+        connect(m, cursor.pos, vgs.pos)
+
+        output = platform.request("vga")
+        # delay hs, vs by 1 clock and use the delayed active signal too
+        m.d.sync += [
+            output.hs.eq(vgs.hs),
+            output.vs.eq(vgs.vs),
+        ]
+        m.d.comb += [
+            output.r.eq(Mux(active1, color[0], 0)),
+            output.g.eq(Mux(active1, color[1], 0)),
+            output.b.eq(Mux(active1, color[2], 0)),
+        ]
+
+        # Not all platforms need the pclk/den outputs, so it's okay if the request fails.
+        try:
+            pclk = platform.request("pclk")
+            m.d.comb += pclk.eq(~ClockSignal("sync"))
+            den = platform.request("den")
+            m.d.sync += den.eq(vgs.active)
+        except:
+            pass
+
+        return m
