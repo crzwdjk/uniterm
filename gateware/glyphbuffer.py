9b8beaf484eebe62beb837b28ba5b69699cadb50
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2023-09-06 19:31:42 -0400

    Add SPRAM support to glyph buffer
    On iCE40 UP platforms, the SPRAM is used for the glyphbuffer, with
    arbitration between reads and writes.

--- gateware/glyphbuffer.py
+++ gateware/glyphbuffer.py
@@ -26,24 +26,56 @@ class GlyphBuffer(Elaboratable):
 
         # memory size/stride rounded up to powers of 2, we can afford it with
         # SPRAM.
+        mem_dataout = Signal(16, reset = 0)
         memsize = (1 << self.read.row.width) * (1 << self.read.col.width)
-        mem = Memory(width = 16, depth = memsize)
-        m.submodules.mem_rd = mem_rd = mem.read_port(transparent = False)
-        m.submodules.mem_wr = mem_wr = mem.write_port()
+        if platform and platform.device == "iCE40UP5K":
+            assert memsize <= 16384
+            addr = Signal(14)
+            mem_wren = Signal(reset = 0)
+            with m.FSM():
+                with m.State("IDLE"):
+                    with m.If(self.read.en):
+                        m.d.comb += addr.eq(Cat(self.read.row, self.read.col))
+                        m.next = "READ"
+                    with m.Elif(self.write.en):
+                        m.d.comb += addr.eq(Cat(self.write.row, self.write.col))
+                        m.d.comb += mem_wren.eq(1)
+                        m.d.comb += self.write.ack.eq(mem_wren)
+                with m.State("READ"):
+                    m.d.comb += addr.eq(Cat(self.read.row, self.read.col))
+                    m.d.sync += self.read.data.eq(mem_dataout)
+                    m.next = "IDLE"
+            m.submodules += Instance("SB_SPRAM256KA",
+                i_ADDRESS = addr,
+                i_DATAIN = self.write.data,
+                i_MASKWREN = Cat(mem_wren, mem_wren, mem_wren, mem_wren),
+                i_WREN = mem_wren,
+                i_CHIPSELECT = Const(1),
+                i_CLOCK = ClockSignal("sync"),
+                i_STANDBY = Const(0),
+                i_SLEEP = Const(0),
+                i_POWEROFF = Const(1),
+                o_DATAOUT = mem_dataout,
+            )
+        else:
+            mem = Memory(width = 16, depth = memsize)
+            m.submodules.mem_rd = mem_rd = mem.read_port(transparent = False)
+            m.submodules.mem_wr = mem_wr = mem.write_port()
 
-        addr = Signal.like(mem_rd.addr)
-        m.d.comb += [
-            mem_rd.en.eq(self.read.en),
-            addr.eq(Mux(self.read.en, Cat(self.read.row, self.read.col),
-                                      Cat(self.write.row, self.write.col))),
-            self.read.data.eq(mem_rd.data),
-            mem_wr.data.eq(self.write.data),
-            mem_rd.addr.eq(addr),
-            mem_wr.addr.eq(addr),
+            addr = Signal.like(mem_rd.addr)
+            m.d.comb += [
+                mem_rd.en.eq(self.read.en),
+                addr.eq(Mux(self.read.en, Cat(self.read.row, self.read.col),
+                                          Cat(self.write.row, self.write.col))),
+                self.read.data.eq(mem_rd.data),
+                mem_wr.data.eq(self.write.data),
+                mem_rd.addr.eq(addr),
+                mem_wr.addr.eq(addr),
 
-            mem_wr.en.eq(self.write.en & ~self.read.en),
-            self.write.ack.eq(self.write.en & ~self.read.en),
-        ]
+                mem_wr.en.eq(self.write.en & ~self.read.en),
+                self.write.ack.eq(self.write.en & ~self.read.en),
+            ]
+
         return m
 
 
@@ -63,12 +95,12 @@ class CharGen(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        ctr = Signal(22)
+        ctr = Signal(21)
         charidx = Signal(16)
-        nextval = Signal(23)
+        nextval = Signal(22)
         m.d.comb += nextval.eq(ctr + 1)
         m.d.comb += self.data.eq(charidx)
-        with m.If(nextval[22]):
+        with m.If(nextval[21]):
             m.d.sync += ctr.eq(0)
             with m.If(charidx == 5318):
                 m.d.sync += charidx.eq(0)
@@ -85,7 +117,7 @@ class CharGen(Elaboratable):
                 m.d.sync += self.col.eq(self.col + 1)
             m.d.sync += self.en.eq(1)
         with m.Else():
-            m.d.sync += ctr.eq(nextval[0:23])
+            m.d.sync += ctr.eq(nextval[0:21])
 
         with m.If(self.en & self.ack):
             m.d.sync += self.en.eq(0)
